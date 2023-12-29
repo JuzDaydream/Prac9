10b0ea4be1b84c39bb41480d9f7d4aa40cceb81d
juzdaydream <juzdaydream@gmail.com>
2023-12-29 11:09:37 +0800

    Add project files.

---
+++ Prac9 (Materials)/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Prac9__Materials_
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        void Application_Error(object sender, Exception e)
+        {
+            Application.Lock();
+            Application["ErrorMsg"] += Server.GetLastError().Message;
+            Application.UnLock();
+        }
+    }
+}
